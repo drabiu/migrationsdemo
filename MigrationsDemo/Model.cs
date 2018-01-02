cfd7a893421ad723d0e138c4e966d3d7af67811b
mdrab <mateusz.drab@dfg.com>
2018-01-02 14:15:18 +0100

    hgy2

---
+++ MigrationsDemo/Model.cs
@@ -0,0 +1,18 @@
+﻿using System.Data.Entity;
+
+namespace MigrationsDemo
+{
+    public class Model
+    {
+        public class BlogContext : DbContext
+        {
+            public DbSet<Blog> Blogs { get; set; }
+        }
+
+        public class Blog
+        {
+            public int BlogId { get; set; }
+            public string Name { get; set; }
+        }
+    }
+}
